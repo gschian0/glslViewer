bf382eb8b4d63fd25ec8e131eeaac9cc902e58c1
gschian0 <gschiano@me.com>
2021-05-24 19:32:15 -0400

    t

--- src/io/fs.cpp
+++ src/io/fs.cpp
@@ -65,6 +65,7 @@ std::string getAbsPath(const std::string& _path) {
 
 std::string urlResolve(const std::string& _path, const std::string& _pwd, const List &_include_folders) {
     std::string url = _pwd +'/'+ _path;
+    std::string url = _pwd +'\\'+ _path;
 
     // If the path is not in the same directory
     if (urlExists(url))
@@ -75,12 +76,16 @@ std::string urlResolve(const std::string& _path, const std::string& _pwd, const 
         for ( uint32_t i = 0; i < _include_folders.size(); i++) {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             std::string new_path = _include_folders[i] + "\\" + _path;
             if (urlExists(new_path))
 =======
 =======
 >>>>>>> parent of 514f958 (test)
             std::string new_path = _include_folders[i] + "/" + _path;
+=======
+            std::string new_path = _include_folders[i] + "\\" + _path;
+>>>>>>> parent of d0b6f54 (lygia includes working)
             if (urlExists(new_path)) 
 >>>>>>> parent of 514f958 (test)
                 return realpath(new_path.c_str(), NULL);
