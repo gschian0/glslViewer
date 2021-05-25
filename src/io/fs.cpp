ed5d041202cd44c247179a7c5e301533cfaab48c
gschian0 <gschiano@me.com>
2021-05-24 20:04:33 -0400

    revert

--- src/io/fs.cpp
+++ src/io/fs.cpp
@@ -66,10 +66,13 @@ std::string getAbsPath(const std::string& _path) {
 std::string urlResolve(const std::string& _path, const std::string& _pwd, const List &_include_folders) {
     std::string url = _pwd +'/'+ _path;
 <<<<<<< HEAD
+<<<<<<< HEAD
     std::string url = _pwd +'\\'+ _path;
 =======
 >>>>>>> parent of 514f958 (test)
     std::string url = _pwd +'\\'+ _path;
+=======
+>>>>>>> parent of 514f958 (test)
 
     // If the path is not in the same directory
     if (urlExists(url))
@@ -78,6 +81,7 @@ std::string urlResolve(const std::string& _path, const std::string& _pwd, const 
     // .. search on the include path
     else {
         for ( uint32_t i = 0; i < _include_folders.size(); i++) {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -97,6 +101,9 @@ std::string urlResolve(const std::string& _path, const std::string& _pwd, const 
 =======
             std::string new_path = _include_folders[i] + "\\" + _path;
 >>>>>>> parent of 8fd4613 (Revert "test2")
+=======
+            std::string new_path = _include_folders[i] + "/" + _path;
+>>>>>>> parent of 514f958 (test)
             if (urlExists(new_path)) 
 >>>>>>> parent of 514f958 (test)
                 return realpath(new_path.c_str(), NULL);
