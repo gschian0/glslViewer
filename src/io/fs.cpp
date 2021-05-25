57092539557ee3bb9c923fd2134aa4481476e35a
gschian0 <gschiano@me.com>
2021-05-24 20:40:35 -0400

    current
    current

--- src/io/fs.cpp
+++ src/io/fs.cpp
@@ -66,6 +66,7 @@ std::string getAbsPath(const std::string& _path) {
 std::string urlResolve(const std::string& _path, const std::string& _pwd, const List &_include_folders) {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     std::string url = _pwd +'/'+ _path;
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -84,6 +85,9 @@ std::string urlResolve(const std::string& _path, const std::string& _pwd, const 
 =======
     std::string url = _pwd +'/'+ _path;
 >>>>>>> parent of 514f958 (test)
+=======
+    std::string url = _pwd +'/'+ _path;
+>>>>>>> parent of 514f958 (test)
 
     // If the path is not in the same directory
     if (urlExists(url))
@@ -92,6 +96,7 @@ std::string urlResolve(const std::string& _path, const std::string& _pwd, const 
     // .. search on the include path
     else {
         for ( uint32_t i = 0; i < _include_folders.size(); i++) {
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -119,6 +124,9 @@ std::string urlResolve(const std::string& _path, const std::string& _pwd, const 
 =======
             std::string new_path = _include_folders[i] + "\\" + _path;
 >>>>>>> parent of 8fd4613 (Revert "test2")
+=======
+            std::string new_path = _include_folders[i] + "/" + _path;
+>>>>>>> parent of 514f958 (test)
 =======
             std::string new_path = _include_folders[i] + "/" + _path;
 >>>>>>> parent of 514f958 (test)
